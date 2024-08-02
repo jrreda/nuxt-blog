4af2f63329e928964e1d3312a3ef206e8771f097
Mahmoud Reda <mahmoudreda457@gmail.com>
2024-08-02 20:31:57 +0300

    dynamic posts

--- components/PostItem.vue
+++ components/PostItem.vue
@@ -2,28 +2,34 @@
     <div class="post-container py-8">
         <!-- Post Title -->
         <h3 class="text-3xl font-bold">
-            <NuxtLink :to="`/posts/1`">
-                Title of Post
+            <NuxtLink :to="`/posts/${props.post.id}`">
+                {{ props.post.title }}
             </NuxtLink>
         </h3>
 
         <!-- Post Meta -->
         <div class="post-meta text-gray-700 flex items-center space-x-2">
-            <div>Jul 5, 2024</div>
+            <div>{{ props.post.created_at }}</div>
             <div>&middot;</div>
-            <div>Mahmoud Reda</div>
+            <div>{{ props.post.user.name }}</div>
         </div>
 
         <!-- Post Preview -->
         <div class="post-preview leading-relaxed mt-4">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit voluptatem, illo earum et sapiente voluptates dolorem alias? Ducimus, tempora? Pariatur asperiores harum necessitatibus earum, doloremque delectus tenetur magni minima blanditiis ullam, ipsa sequi! Qui recusandae excepturi, voluptate animi suscipit ut?
+            {{ props.post.body }}
         </div>
 
         <!-- Read more -->
         <div class="mt-4">
-            <NuxtLink :to="`/posts/1`" class="bg-blue-700 hover:bg-blue-800 text-white rounded inline-block px-4 py-2">
+            <NuxtLink :to="`/posts/${props.post.id}`" class="bg-blue-700 hover:bg-blue-800 text-white rounded inline-block px-4 py-2">
                 Read More
             </NuxtLink>
         </div>
     </div>
 </template>
+
+<script setup>
+const props = defineProps({
+    post: Object,
+});
+</script>
--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -1,29 +1,32 @@
 <template>
     <div class="container mx-auto w-1/2 py-8">
         <!-- Page Title -->
-        <Title>{{ $route.params.id }} | {{ title }}</Title>
+        <Title>{{ post.title }} | {{ title }}</Title>
 
         <!-- Post Title -->
         <h3 class="text-3xl font-bold">
             <NuxtLink :to="`/posts/1`">
-                Title of Post
+                {{ post.title }}
             </NuxtLink>
         </h3>
 
         <!-- Post Meta -->
         <div class="post-meta text-gray-700 flex items-center space-x-2">
-            <div>Jul 5, 2024</div>
+            <div>{{ post.created_at }}</div>
             <div>&middot;</div>
-            <div>Mahmoud Reda</div>
+            <div>{{ post.user.name }}</div>
         </div>
 
         <!-- Post Content -->
         <div class="post-content mt-4">
-            {{ $route.params.id }} Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis qui, esse aspernatur corrupti quidem provident eligendi dolores quasi, dignissimos quisquam sapiente quaerat quibusdam, ullam enim iste nemo. Dicta minus doloremque, quisquam provident architecto tempora aperiam doloribus. Repellat id facere quam omnis natus maiores aut dolorum, eligendi, laboriosam totam eaque. Reprehenderit dolores repellat fuga ratione quo id non, eum facilis dolore necessitatibus? Odit id a fugiat, aspernatur nihil neque facere excepturi! Commodi magnam veritatis adipisci aspernatur nisi quod, corrupti maxime accusamus nobis officiis recusandae eaque ad iure debitis odit voluptate nostrum voluptatem nemo exercitationem sapiente reiciendis cum ea amet. Deserunt, perspiciatis.
+            {{ $route.params.id }} {{ post.body }}
         </div>
     </div>
 </template>
 
 <script setup>
     const title = useState('title');
+    const route = useRoute();
+
+    const { data: post } = await useFetch(`http://nuxt-backend.test/api/post/${route.params.id}`);
 </script>
